e64cb62ec5c1144ace1b597d72046d3b2b22b8b1
dgqyushen <2446682260@qq.com>
2021-01-26 22:38:13 +0800

    完善了用户的功能

---
+++ src/main/java/com/qian/springbootlibrary/vo/ViewUser.java
@@ -0,0 +1,18 @@
+package com.qian.springbootlibrary.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ViewUser {
+    private int id;
+    private String username;
+    private String password;
+    private boolean root;
+    private String city;
+    private String email;
+    private boolean sex;
+}
